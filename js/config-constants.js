c5e63eef0f84c258176eaa85eee7ac12b77867ce
Mackenzie May <mack340@proton.me>
2025-09-23 20:15:50 +1000

    add scenario comparison, remove au-filter+dep. heatmap

--- js/config-constants.js
+++ js/config-constants.js
@@ -1,7 +1,7 @@
 const TCConfig = {
     app: {
         name: 'Tropical Cyclone Track Visualization',
-        version: '0.13.0',
+        version: '0.14.0',
         description: 'd4PDF Climate Model Projections for Australia',
         author: 'Team 7 Sharks',
         buildDate: '2024-08-21'
