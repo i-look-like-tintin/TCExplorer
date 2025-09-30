0cf1e76177478af03a4d89a0b1a98fcccd51b1d2
Mackenzie May <mack340@proton.me>
2025-09-30 20:21:28 +1000

    iOS map error fixed
    (for me at least, pending additional device testing)

--- js/config-constants.js
+++ js/config-constants.js
@@ -1,7 +1,7 @@
 const TCConfig = {
     app: {
         name: 'Tropical Cyclone Track Visualization',
-        version: '1.0.0a',
+        version: '1.0.2a',
         description: 'd4PDF Climate Model Projections for Australia',
         author: 'Team 7 Sharks',
         buildDate: '2025-09-29'
