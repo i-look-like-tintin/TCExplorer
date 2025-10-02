769e0f4b7e65a75d611ba93a0c471766db2f88d6
Mackenzie May <mack340@proton.me>
2025-10-02 14:56:20 +1000

    test performance fix iOS
    Set performance level to 'high' instead of assessing it dynamically.

--- js/device-manager.js
+++ js/device-manager.js
@@ -16,7 +16,7 @@ class DeviceManager {
         this.deviceType = this.detectDeviceType();
         this.orientation = this.getOrientation();
         this.touchSupport = this.detectTouchSupport();
-        this.performanceLevel = this.assessPerformanceLevel(); //'high' 
+        this.performanceLevel = 'high'; //this.assessPerformanceLevel();
         this.controlPanelExpanded = false;
         
         this.init();
